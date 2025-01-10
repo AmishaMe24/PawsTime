7e6c0b47208b84ca2abc82a46e1539319a312819
Amisha Mehta <amishamehta24@gmail.com>
2025-01-10 17:52:18 -0500

    updated index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // allow cross-origin requests
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: '',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
