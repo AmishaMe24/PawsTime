8974ca9aac391a72f711f33a91d67297f8d88adf
Amisha Mehta <amishamehta24@gmail.com>
2025-01-10 18:11:29 -0500

    updated cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // allow cross-origin requests
 const corsOptions = {
-  origin: '',
+  origin: 'https://paws-time-client.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
